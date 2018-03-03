ede0a454e7d5cf95d32af8980ee786c42b0496f6
Philroy <philroy.williams@mymona.uwi.edu>
2018-03-03 22:53:20 +0000

    step9

--- app/views.py
+++ app/views.py
@@ -27,28 +27,50 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
+
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == 'POST' and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
-
-            # using your model, query database for a user based on the username
-            # and password submitted
-            # store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method.
-
-            # get user id, load into session
+        
+        # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
+        # using your model, query database for a user based on the username
+        # and password submitted
+        # store the result of that query to a `user` variable so it can be
+        # passed to the login_user() method.
+        user = UserProfile.query.filter_by(username=username).first()
+        
+        if user.password == password:
+             # get user id, load into session
             login_user(user)
-
+            flash('Logged in successfully.', 'success')
+            
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for("secure") )
+    
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            # they should be redirected to a secure-page route instead 
+    flash_errors(form)
     return render_template("login.html", form=form)
 
+# Flash errors from the form if validation fails
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
